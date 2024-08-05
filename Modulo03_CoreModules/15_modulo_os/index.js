1d9f00f9c5df128a38da64ebb6eb97b2a130cedc
JoseBarceloss <barcelosps41710@gmail.com>
2024-08-05 12:45:46 -0300

    Aprendi usar o modulo OS para saber dados do servidor

---
+++ Modulo03_CoreModules/15_modulo_os/index.js
@@ -0,0 +1,9 @@
+const os = require('os')
+
+console.log(os.cpus()) // quantas CPUS tem no servidor
+
+console.log(os.freemem()) // quantas de memoria livre tem na maquina mostrado em MB
+
+console.log(os.homedir()) // qual diretorio da home 
+
+console.log(os.type()) // qual o tipo de sistema operacional roda no servidor
